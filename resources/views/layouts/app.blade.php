6dc0017b40e3ef0d70d5d197de19c4940ad761e0
i65 <2461585309@qq.com>
2019-03-13 02:23:09 +0000

    基础布局

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- csrf-token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title', 'Laravel Shop') - Laravel 电商教程 </title>
+    <!-- 样式 -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    <div id="app" class="{{ route_class() }}-page">
+        @include('layouts._header')
+        <div class="container">
+            @yield('content')
+        </div>
+        @include('layouts._footer')
+    </div>
+    <!-- js 脚本 -->
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,7 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+// 在末尾加了一个 version()，使 Mix 每次生成的静态文件后面加上一个类似版本号的参数，避免浏览器缓存
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .version();
