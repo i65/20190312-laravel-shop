2346a0cf08c58df85a3d8628801d1bb1ab89ec2d
i65 <2461585309@qq.com>
2019-03-22 09:51:09 +0000

    收货地址列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,4 +37,11 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function addresses()
+    {
+        return $this->hasMany(UserAddress::class);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Auth::routes();
 // Route::get('/home', 'HomeController@index')->name('home');
 // 在之前的路由里加上一个 verify 参数
 Auth::routes(['verify' => true]);
+
+// auth 中间件代表需要登录，verified 中间件代表需要经过邮箱验证
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
