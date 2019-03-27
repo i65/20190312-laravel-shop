35229e189a95b8c64f3b51523b9c2ad74c11a79c
i65 <2461585309@qq.com>
2019-03-27 08:40:38 +0000

    异常处理

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\InvalidRequestException;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -57,7 +58,7 @@ class ProductsController extends Controller
     {
         // 判断商品是否已上架，如果没有上架则抛出异常
         if (!$product->on_sale) {
-            throw new \Exception('商品未上架');
+            throw new InvalidRequestException('商品未上架');
         }
 
         return view('products.show', ['product' => $product]);
